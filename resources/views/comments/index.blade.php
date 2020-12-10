950aa54ed024ec4d327e85daf0c734232dc339b1
wngur6076 <wngur6076@naver.com>
2020-12-10 21:56:07 +0900

    댓글 커맨드뷰

---
+++ resources/views/comments/index.blade.php
@@ -0,0 +1,26 @@
+@php
+    $currentUser = auth()->user();
+    $comments = $article->comments;
+@endphp
+
+<div id="page-header">
+    <h4>댓글</h4>
+</div>
+
+<div id="form__new__comment">
+    @if ($currentUser)
+        {{-- @include('comments.partial.create') --}}
+    @else
+        {{-- @include('comments.partial.login') --}}
+    @endif
+</div>
+
+<div id="list__comment">
+    @forelse ($comments as $comment)
+        @include('comments.partial.comment', [
+            'parentId' => $comment->id,
+            'isReply' => false,
+        ])
+    @empty
+    @endforelse
+</div>
